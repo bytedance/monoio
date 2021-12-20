b51d4efd0b294740ddf9b1afe7bd66dcb4a8680c
ihc童鞋@提不起劲 <ihciah@gmail.com>
2021-12-20 20:13:26 +0800

    chore: fix warnings (#29)

--- monoio/src/net/listener_config.rs
+++ monoio/src/net/listener_config.rs
@@ -27,30 +27,35 @@ impl Default for ListenerConfig {
 
 impl ListenerConfig {
     /// Enable SO_REUSEPORT
+    #[must_use]
     pub fn reuse_port(mut self, reuse_port: bool) -> Self {
         self.reuse_port = reuse_port;
         self
     }
 
     /// Enable SO_REUSEADDR
+    #[must_use]
     pub fn reuse_addr(mut self, reuse_addr: bool) -> Self {
         self.reuse_addr = reuse_addr;
         self
     }
 
     /// Specify backlog
+    #[must_use]
     pub fn backlog(mut self, backlog: i32) -> Self {
         self.backlog = backlog;
         self
     }
 
     /// Specify SO_SNDBUF
+    #[must_use]
     pub fn send_buf_size(mut self, send_buf_size: usize) -> Self {
         self.send_buf_size = Some(send_buf_size);
         self
     }
 
     /// Specify SO_RCVBUF
+    #[must_use]
     pub fn recv_buf_size(mut self, recv_buf_size: usize) -> Self {
         self.recv_buf_size = Some(recv_buf_size);
         self
