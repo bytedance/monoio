3808050dc0196ff9df39fb9ef1fc7916a5d33c4d
ihciah <ihciah@gmail.com>
2021-12-22 21:00:26 +0800

    feat: add async buf read trait and BufReader

--- monoio/src/io/mod.rs
+++ monoio/src/io/mod.rs
@@ -5,7 +5,11 @@ mod async_read_rent_ext;
 mod async_write_rent;
 mod async_write_rent_ext;
 
+mod async_buf_read;
+
 pub use async_read_rent::{AsyncReadRent, AsyncReadRentAt};
 pub use async_read_rent_ext::AsyncReadRentExt;
 pub use async_write_rent::{AsyncWriteRent, AsyncWriteRentAt};
 pub use async_write_rent_ext::AsyncWriteRentExt;
+
+pub use async_buf_read::{AsyncBufRead, BufReader};
