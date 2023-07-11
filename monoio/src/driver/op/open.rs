f891b7058158e2687a4c827e89fc6fda71d57793
Jin Wei Tan <48111396+CarrotzRule123@users.noreply.github.com>
2023-07-11 11:03:33 +0800

    implement open op on windows (#189)

--- monoio/src/driver/op/open.rs
+++ monoio/src/driver/op/open.rs
@@ -2,18 +2,27 @@ use std::{ffi::CString, io, path::Path};
 
 #[cfg(all(target_os = "linux", feature = "iouring"))]
 use io_uring::{opcode, types};
+#[cfg(windows)]
+use windows_sys::Win32::{Foundation::INVALID_HANDLE_VALUE, Storage::FileSystem::CreateFileW};
 
 use super::{Op, OpAble};
+#[cfg(feature = "legacy")]
+use crate::driver::legacy::ready::Direction;
+#[cfg(windows)]
+use crate::syscall;
 #[cfg(all(unix, feature = "legacy"))]
-use crate::{driver::legacy::ready::Direction, syscall_u32};
+use crate::syscall_u32;
 use crate::{driver::util::cstr, fs::OpenOptions};
 
 /// Open a file
 pub(crate) struct Open {
     pub(crate) path: CString,
+    #[cfg(unix)]
     flags: i32,
     #[cfg(unix)]
     mode: libc::mode_t,
+    #[cfg(windows)]
+    opts: OpenOptions,
 }
 
 impl Op<Open> {
@@ -30,6 +39,18 @@ impl Op<Open> {
 
         Op::submit_with(Open { path, flags, mode })
     }
+
+    #[cfg(windows)]
+    /// Submit a request to open a file.
+    pub(crate) fn open<P: AsRef<Path>>(path: P, options: &OpenOptions) -> io::Result<Op<Open>> {
+        // Here the path will be copied, so its safe.
+        let path = cstr(path.as_ref())?;
+
+        Op::submit_with(Open {
+            path,
+            opts: options.clone(),
+        })
+    }
 }
 
 impl OpAble for Open {
@@ -41,7 +62,7 @@ impl OpAble for Open {
             .build()
     }
 
-    #[cfg(all(unix, feature = "legacy"))]
+    #[cfg(feature = "legacy")]
     fn legacy_interest(&self) -> Option<(Direction, usize)> {
         None
     }
@@ -53,5 +74,22 @@ impl OpAble for Open {
             self.flags,
             self.mode as libc::c_int
         ))
+    }
+
+    #[cfg(windows)]
+    fn legacy_call(&mut self) -> io::Result<u32> {
+        syscall!(
+            CreateFileW(
+                self.path.as_c_str().as_ptr(),
+                self.opts.access_mode()?,
+                self.opts.share_mode,
+                self.opts.security_attributes,
+                self.opts.creation_mode()?,
+                self.opts.get_flags_and_attributes(),
+                std::ptr::null_mut(),
+            ),
+            PartialEq::eq,
+            INVALID_HANDLE_VALUE
+        )
     }
 }
