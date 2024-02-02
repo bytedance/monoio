5988112915969f9eeeb165d7a71ad040a1d7f27e
dragon-zhang <zhangzicheng@apache.org>
2024-02-02 18:33:34 +0800

    update CI

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -18,11 +18,26 @@ export RUST_BACKTRACE=1
 # test monoio mod
 cd "${PROJECT_DIR}"/monoio
 
-"${CARGO}" test --target "${TARGET}"
-"${CARGO}" test --target "${TARGET}" --release
+# only enable legacy driver
+"${CARGO}" test --target "${TARGET}" --no-default-features --features "async-cancel,bytes,legacy,macros,utils"
+"${CARGO}" test --target "${TARGET}" --no-default-features --features "async-cancel,bytes,legacy,macros,utils" --release
 
-"${CARGO}" test --target "${TARGET}" --all-features
-"${CARGO}" test --target "${TARGET}" --all-features --release
+if [ "${TARGET}" = "x86_64-unknown-linux-gnu" ]; then
+
+    # only enabled uring driver
+    "${CARGO}" test --target "${TARGET}" --no-default-features --features "async-cancel,bytes,iouring,macros,utils"
+    "${CARGO}" test --target "${TARGET}" --no-default-features --features "async-cancel,bytes,iouring,macros,utils" --release
+
+    # enable uring+legacy driver
+    "${CARGO}" test --target "${TARGET}"
+    "${CARGO}" test --target "${TARGET}" --release
+
+    if [ "${CHANNEL}" == "nightly" ]; then
+        "${CARGO}" test --target "${TARGET}" --all-features
+        "${CARGO}" test --target "${TARGET}" --all-features --release
+    fi
+
+fi
 
 # test monoio-compat mod
 cd "${PROJECT_DIR}"/monoio-compat
@@ -30,5 +45,7 @@ cd "${PROJECT_DIR}"/monoio-compat
 "${CARGO}" test --target "${TARGET}"
 "${CARGO}" test --target "${TARGET}" --release
 
-"${CARGO}" test --target "${TARGET}" --all-features
-"${CARGO}" test --target "${TARGET}" --all-features --release
+if [ "${CHANNEL}" == "nightly" ]; then
+    "${CARGO}" test --target "${TARGET}" --all-features
+    "${CARGO}" test --target "${TARGET}" --all-features --release
+fi
