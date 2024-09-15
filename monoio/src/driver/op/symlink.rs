f10098ca774e6d6ca369e060cee379ddd9cce7d0
misssondeer <819643718@qq.com>
2024-09-15 18:47:56 +0800

    rename symlink structure field

--- monoio/src/driver/op/symlink.rs
+++ monoio/src/driver/op/symlink.rs
@@ -4,17 +4,17 @@ use super::{Op, OpAble};
 use crate::driver::util::cstr;
 
 pub(crate) struct Symlink {
-    pub(crate) _from: CString,
-    pub(crate) _to: CString,
+    pub(crate) from: CString,
+    pub(crate) to: CString,
 }
 impl Op<Symlink> {
     pub(crate) fn symlink<P: AsRef<Path>, Q: AsRef<Path>>(
         from: P,
         to: Q,
     ) -> io::Result<Op<Symlink>> {
-        let _from = cstr(from.as_ref())?;
-        let _to = cstr(to.as_ref())?;
-        Op::submit_with(Symlink { _from, _to })
+        let from = cstr(from.as_ref())?;
+        let to = cstr(to.as_ref())?;
+        Op::submit_with(Symlink { from, to })
     }
 }
 
@@ -22,8 +22,8 @@ impl OpAble for Symlink {
     #[cfg(all(target_os = "linux", feature = "iouring"))]
     fn uring_op(&mut self) -> io_uring::squeue::Entry {
         use io_uring::{opcode, types};
-        let from_ref = self._from.as_c_str().as_ptr();
-        let to_ref = self._to.as_c_str().as_ptr();
+        let from_ref = self.from.as_c_str().as_ptr();
+        let to_ref = self.to.as_c_str().as_ptr();
         opcode::SymlinkAt::new(types::Fd(libc::AT_FDCWD), from_ref, to_ref).build()
     }
 
@@ -36,8 +36,8 @@ impl OpAble for Symlink {
     fn legacy_call(&mut self) -> std::io::Result<u32> {
         use crate::syscall_u32;
         syscall_u32!(symlink(
-            self._from.as_c_str().as_ptr(),
-            self._to.as_c_str().as_ptr()
+            self.from.as_c_str().as_ptr(),
+            self.to.as_c_str().as_ptr()
         ))
     }
 }
